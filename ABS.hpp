a4fb916ac846ad7668396c3ab4b7ad172d48fc3f
TheLogicle <70608147+TheLogicle@users.noreply.github.com>
2025-11-06 01:48:14 -0500

    abs

--- ABS.hpp
+++ ABS.hpp
@@ -14,8 +14,8 @@ class ABS : public StackInterface<T> {
 		// Big 5 + Parameterized Constructor
 		ABS()
 		{
-			array_ = new T[0];
-			capacity_ = 0;
+			array_ = new T[1];
+			capacity_ = 1;
 			curr_size_ = 0;
 		}
 
