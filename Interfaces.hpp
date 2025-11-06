ac4c72291cc8325796fde1356741391ac16475a5
TheLogicle <70608147+TheLogicle@users.noreply.github.com>
2025-11-05 23:36:30 -0500

    interfaces

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -5,19 +5,50 @@
 
 
 template <typename T>
-class StackInterface {
+class StackInterface
+{
+
+	void push (const T& el);
+	T pop ();
+	T peek () const;
+
+	std::size_t getSize () const noexcept;
+
+	virtual ~StackInterface () = 0;
 
 };
 
 
 template <typename T>
-class QueueInterface {
+class QueueInterface
+{
 
+	void enqueue (const T& el);
+	T dequeue ();
+	T peek () const;
+
+	std::size_t getSize () const noexcept;
+
+	virtual ~QueueInterface () = 0;
 };
 
 
 template <typename T>
-class DequeInterface {
+class DequeInterface
+{
+
+	void pushFront (const T& el);
+	void pushBack (const T& el);
+
+	T popFront ();
+	T popBack ();
+	
+	const T& front () const;
+	const T& back () const;
+
+	std::size_t getSize () const noexcept;
+
+	virtual ~DequeInterface () = 0;
 
 };
 
