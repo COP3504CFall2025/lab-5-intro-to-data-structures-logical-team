9b75ff7d4f9fc6f8412d3f91185ab35b4e1190b8
TheLogicle <70608147+TheLogicle@users.noreply.github.com>
2025-11-05 19:42:17 -0500

    linked list

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -1,47 +1,201 @@
 #pragma once
 #include <iostream>
-using namespace std;
-
-
 
 template <typename T>
-class LinkedList {
-public:
-	// Behaviors
-	void printForward() const;
-	void printReverse() const;
+class LinkedList
+{
 
-	// Accessors
-	[[nodiscard]] unsigned int getCount() const;
-	Node* getHead();
-	const Node* getHead() const;
-	Node* getTail();
-	const Node* getTail() const;
+	public:
 
-	// Insertion
-	void addHead(const T& data);
-	void addTail(const T& data);
+		struct Node
+		{
+			T data;
+			Node* prev;
+			Node* next;
+		};
 
-	// Removal
-	bool removeHead();
-	bool removeTail();
-	void Clear();
+		// Behaviors
+		void printForward() const
+		{
+			for (Node* el = head; el != nullptr; el = el->next)
+			{
+				std::cout << el->data << std::endl;
+			}
+		}
+		void printReverse() const
+		{
+			for (Node* el = tail; el != nullptr; el = el->prev)
+			{
+				std::cout << el->data << std::endl;
+			}
+		}
 
-	// Operators
-	LinkedList<T>& operator=(LinkedList<T>&& other) noexcept;
-	LinkedList<T>& operator=(const LinkedList<T>& rhs);
+		// Accessors
+		[[nodiscard]] unsigned int getCount() const
+		{
+			return count;
+		}
+		Node* getHead()
+		{
+			return head;
+		}
+		const Node* getHead() const
+		{
+			return head;
+		}
+		Node* getTail()
+		{
+			return tail;
+		}
+		const Node* getTail() const
+		{
+			return tail;
+		}
 
-	// Construction/Destruction
-	LinkedList();
-	LinkedList(const LinkedList<T>& list);
-	LinkedList(LinkedList<T>&& other) noexcept;
-	~LinkedList();
+		// Insertion
+		void addHead(const T& data)
+		{
+			Node* prevHead = head;
+			head = new Node{.data = data, .prev = nullptr, .next = prevHead};
 
-private:
-	// Stores pointers to first and last nodes and count
-	Node* head;
-	Node* tail;
-	unsigned int count;
+			// check if allocation failed
+			if (!head)
+			{
+				head = prevHead;
+				throw std::runtime_error("could not insert at head");
+			}
+
+			++count;
+
+			if (prevHead) prevHead->prev = head;
+		}
+		void addTail(const T& data)
+		{
+			Node* prevTail = tail;
+			tail = new Node{.data = data, .prev = prevTail, .next = nullptr};
+
+			// check if allocation failed
+			if (!tail)
+			{
+				tail = prevTail;
+				throw std::runtime_error("could not insert at tail");
+			}
+
+			++count;
+
+			if (prevTail) prevTail->next = tail;
+		}
+
+		// Removal
+		bool removeHead()
+		{
+			if (!head)
+			{
+				throw std::runtime_error("cannot remove head from empty linked list");
+			}
+
+			Node* removable = head;
+			Node* newHead = removable->next;
+
+			if (newHead)
+			{
+				newHead->prev = nullptr;
+			}
+
+			head = newHead;
+
+			--count;
+
+			delete removable;
+		}
+		bool removeTail()
+		{
+			if (!tail)
+			{
+				throw std::runtime_error("cannot remove tail from empty linked list");
+			}
+
+			Node* removable = tail;
+			Node* newTail = removable->prev;
+
+			if (newTail)
+			{
+				newTail->next = nullptr;
+			}
+
+			tail = newTail;
+
+			--count;
+
+			delete removable;
+		}
+		void Clear()
+		{
+			while (head) removeHead();
+		}
+
+		// Operators
+		LinkedList<T>& operator=(LinkedList<T>&& other) noexcept
+		{
+			return m_move(other);
+		}
+		LinkedList<T>& operator=(const LinkedList<T>& other)
+		{
+			return m_copy(other);
+		}
+
+		// Construction/Destruction
+		LinkedList() : head(nullptr), tail(nullptr), count(0) {}
+
+		LinkedList(const LinkedList<T>& other)
+		{
+			m_copy(other);
+		}
+		LinkedList(LinkedList<T>&& other) noexcept
+		{
+			m_move(other);
+		}
+
+		~LinkedList()
+		{
+			Clear();
+		}
+
+	private:
+		// Stores pointers to first and last nodes and count
+		Node* head;
+		Node* tail;
+		unsigned int count;
+
+		LinkedList<T>& m_copy (const LinkedList<T> &other)
+		{
+			if (&other == this) return *this;
+
+			Clear();
+
+			for (Node* el = other.head; el != nullptr; el = el->next)
+			{
+				addTail(el->data);
+			}
+
+			return *this;
+		}
+		LinkedList<T>& m_move (LinkedList<T> &&other) noexcept
+		{
+			if (&other == this) return *this;
+
+			Clear();
+
+			head = other.head;
+			tail = other.tail;
+			count = other.count;
+
+			other.head = nullptr;
+			other.tail = nullptr;
+			other.count = 0;
+
+			return *this;
+		}
 
 };
 
