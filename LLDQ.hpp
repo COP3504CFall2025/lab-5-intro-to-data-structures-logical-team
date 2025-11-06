eccd6e5ad13ecd1536e8be6f267bf9c66072999f
TheLogicle <70608147+TheLogicle@users.noreply.github.com>
2025-11-06 11:36:41 -0500

    lldq

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -7,30 +7,79 @@
 #include <utility>
 
 
-
 template <typename T>
-class LLDQ : public DequeInterface<T> {
-private:
-    LinkedList<T> list;
+class LLDQ : public DequeInterface<T>
+{
+	private:
+		LinkedList<T> list;
 
-public:
-    // Constructor
-    LLDQ();
+	public:
+		// Constructor
+		LLDQ() {}
+		// Default big-five are fine, because this class doesn't directly manage dynamic data
 
-    // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+		void PrintForward ()
+		{
+			list.PrintForward();
+		}
 
-    // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+		void PrintReverse ()
+		{
+			list.PrintReverse();
+		}
 
-    // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+		// Core Insertion Operations
+		void pushFront(const T& el) override
+		{
+			list.AddHead(el);
+		}
 
-    // Getter
-    std::size_t getSize() const noexcept override;
+		void pushBack(const T& el) override
+		{
+			list.AddTail(el);
+		}
+
+		// Core Removal Operations
+		T popFront() override
+		{
+			T* poppedPtr = list.getHead();
+			if (!poppedPtr) throw std::runtime_error("cannot pop from empty deque"); 
+
+			list.RemoveHead();
+
+			return *poppedPtr;
+		}
+		T popBack() override
+		{
+			T* poppedPtr = list.getTail();
+			if (!poppedPtr) throw std::runtime_error("cannot pop from empty deque");
+
+			list.RemoveTail();
+
+			return *poppedPtr;
+		}
+
+		// Element Accessors
+		const T& front() const override
+		{
+			T* frontPtr = list.getHead();
+			if (!frontPtr) throw std::runtime_error("cannot access front of empty deque");
+
+			return *frontPtr;
+		}
+		const T& back() const override
+		{
+			T* backPtr = list.getTail();
+			if (!backPtr) throw std::runtime_error("cannot access back of empty deque");
+
+			return *backPtr;
+		}
+
+		// Getter
+		std::size_t getSize() const noexcept override
+		{
+			return list.getCount();
+		}
 };
 
 
